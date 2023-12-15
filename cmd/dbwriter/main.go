f3f6dba1946bb993ea1bb4f65057040486cbf360
josh <josh@pop-os.localdomain>
2023-12-15 06:39:22 -0700

    Updated code

--- cmd/dbwriter/main.go
+++ cmd/dbwriter/main.go
@@ -2,37 +2,51 @@ package main
 
 import (
 	"context"
-	"log"
+	"encoding/json"
+	"log/slog"
+	"os"
 	"time"
 
 	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
+	"github.com/joho/godotenv"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
+type SensorData struct {
+	Voltage     float64 `json:"voltage"`
+	Current     float64 `json:"current"`
+	Temperature float64 `json:"temperature"`
+	ID          string  `json:"id"`
+}
+
+var (
+	influxDBToken  string
+	influxDBOrg    string
+	influxDBBucket string
+)
+
 const (
-	rabbitMQURL    = "amqp://guest:guest@localhost:5672/"
-	queueName      = "testQueue1"
-	influxDBURL    = "http://localhost:8086"
-	influxDBToken  = "your-influxdb-token"
-	influxDBOrg    = "your-org"
-	influxDBBucket = "your-bucket"
+	rabbitMQURL = "amqp://guest:guest@localhost:5672/"
+	influxDBURL = "http://localhost:8086"
+	queueName   = "testQueue1"
 )
 
 func connectToRabbitMQ() *amqp.Channel {
 	conn, err := amqp.Dial(rabbitMQURL)
 	if err != nil {
-		log.Fatalf("Failed to connect to RabbitMQ: %s", err)
+		slog.Error("Failed to connect to RabbitMQ", "error", err)
 	}
 
 	ch, err := conn.Channel()
 	if err != nil {
-		log.Fatalf("Failed to open a channel: %s", err)
+		slog.Error("Failed to open a channel", "error", err)
 	}
 
 	return ch
 }
 
 func connectToInfluxDB() influxdb2.Client {
+	slog.Info("Connecting to Influx with", "token", influxDBToken, "org", influxDBOrg, "bucket", influxDBBucket)
 	client := influxdb2.NewClient(influxDBURL, influxDBToken)
 	return client
 }
@@ -48,32 +62,48 @@ func consumeAndWrite(ch *amqp.Channel, influxClient influxdb2.Client) {
 		nil,       // args
 	)
 	if err != nil {
-		log.Fatalf("Failed to register a consumer: %s", err)
+		slog.Error("Failed to register a consumer", "error", err)
 	}
 
 	for msg := range msgs {
-		log.Printf("Received a message: %s", msg.Body)
-		writeDataToInfluxDB(influxClient, string(msg.Body))
+		slog.Info("Received a message", "message", msg.Body)
+		var data SensorData
+		err := json.Unmarshal(msg.Body, &data)
+		if err != nil {
+			slog.Error("Could not unmarshal data", "error", err)
+		}
+		writeDataToInfluxDB(influxClient, data)
 	}
 }
 
-func writeDataToInfluxDB(client influxdb2.Client, data string) {
+func writeDataToInfluxDB(client influxdb2.Client, data SensorData) {
 	writeAPI := client.WriteAPIBlocking(influxDBOrg, influxDBBucket)
 
 	// Create a point and add to batch
-	p := influxdb2.NewPointWithMeasurement("measurementName").
-		AddTag("tag", "tagValue").
-		AddField("field", data).
+	p := influxdb2.NewPointWithMeasurement("testSensorData").
+		AddTag("tag", "testSensor1").
+		AddField("id", data.ID).
+		AddField("voltage", data.Voltage).
+		AddField("current", data.Current).
+		AddField("temperature", data.Temperature).
 		SetTime(time.Now())
 
 	// Write the point
 	err := writeAPI.WritePoint(context.Background(), p)
 	if err != nil {
-		log.Fatalf("Failed to write to InfluxDB: %s", err)
+		slog.Error("Failed to write to InfluxDB", "error", err)
 	}
 }
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		slog.Error("Could nod load env file", "error", err)
+	}
+	influxDBToken = os.Getenv("DB_WRITER_TOKEN")
+	influxDBOrg = os.Getenv("INFLUX_ORG")
+	influxDBBucket = os.Getenv("INFLUX_BUCKET")
+
 	rabbitCh := connectToRabbitMQ()
 	defer rabbitCh.Close()
 
@@ -82,3 +112,33 @@ func main() {
 
 	consumeAndWrite(rabbitCh, influxClient)
 }
+
+// func writeDataToInfluxDB(client influxdb2.Client, data SensorData) {
+//     // Create a new point using the measurement name (e.g., "sensor_data")
+//     p := influxdb2.NewPoint("sensor_data",
+//         map[string]string{
+//             "id": data.ID, // Using ID as a tag for querying
+//         },
+//         map[string]interface{}{
+//             "voltage":     data.Voltage,
+//             "current":     data.Current,
+//             "temperature": data.Temperature,
+//         },
+//         time.Now(), // or you might want to use a timestamp from the data if available
+//     )
+
+//     // Get non-blocking write client
+//     writeAPI := client.WriteAPI("your-organization", "your-bucket")
+
+//     // Add data point to the batch
+//     writeAPI.WritePoint(p)
+
+//     // Always check for errors
+//     // Ensure that any buffered data is sent
+//     writeAPI.Flush()
+
+//     // Check if there were any errors during the write
+//     if writeErr := writeAPI.Errors(); writeErr != nil {
+//         log.Fatalf("Write error: %s", writeErr)
+//     }
+// }
